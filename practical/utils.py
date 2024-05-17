4aba7c52b533a904bd6482df604f9f4b6c5012df
moritsih <mor.haderer@gmail.com>
2024-05-17 16:38:46 +0200

    Fixed result presentation

--- practical/utils.py
+++ practical/utils.py
@@ -37,29 +37,31 @@ def compute_dice_score(pred, mask):
 
 
 def make_results_table(model, dataset, device, cfg, model_name):
-
     '''
     Using tabulate, the model is applied to the test dataset and the mean dice score is calculated for each class.
     The results are then saved in a table and returned.
     '''
-
     model.eval()
 
     results = []
 
-    for i, sample in enumerate(tqdm(dataset)):
-        img = sample['img'].unsqueeze(0).to(device)
-        mask = sample['masks'].unsqueeze(0).to(device)
+    with torch.no_grad():  # Ensure no gradients are calculated
+        for i, sample in enumerate(tqdm(dataset)):
+            img = sample['img'].unsqueeze(0).to(device)
+            mask = sample['masks'].unsqueeze(0).to(device)
 
-        pred = model(img)
-        pred = torch.sigmoid(pred)
+            pred = model(img)
+            pred = torch.sigmoid(pred)
 
-        # thresholding
-        pred[pred > 0.5] = 1
-        pred[pred <= 0.5] = 0
+            # thresholding
+            pred[pred > 0.5] = 1
+            pred[pred <= 0.5] = 0
 
-        dice_scores = compute_dice_score(pred, mask)
-        results.append(dice_scores)
+            dice_scores = compute_dice_score(pred, mask)
+            results.append(dice_scores) 
+
+            del img, mask, pred  # Explicitly delete tensors to free memory
+            torch.cuda.empty_cache()  # Free up unused memory
 
     results = np.array(results)
     mean_dice_scores = np.round(np.mean(results, axis=0), 3)
@@ -67,6 +69,7 @@ def make_results_table(model, dataset, device, cfg, model_name):
     table = [model_name, mean_dice_scores[0], mean_dice_scores[1], mean_dice_scores[2]]
 
     return table
+
 
 
 table = [['Model', 'IRF', 'SRF', 'PED']]
